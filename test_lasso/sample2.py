27d93f0ca4a46d40241ed02de4fdfdbcf590237f
torupa <xpt77@outlook.com>
2024-04-24 21:06:00 +0900

    add test lasso

---
+++ test_lasso/sample2.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+p = Path(__file__).parent
+print('p=', p, ' type(p)=', type(p))
+print('p.name=', p.name)
+print('p.is_dir=', p.is_dir(), ' p.is_file=', p.is_file())
+print('---')
+
+p2 = p/Path('hoge')/Path('foo')
+p2 = Path('hoge','foo')
+print('p2=', str(p2))
+print('p2.name=', p2.name)
+print('p2.parts=', p2.parts)
+print('p2.exsits=', p2.exists())
+
